c7f3ba5ef44818b5cd50fc1c09d5f898c80410fe
Babken Vardanyan <babken.vardanyan@sflpro.com>
2017-08-06 16:30:21 +0400

    Created views and urls for challenges

---
+++ challenges/urls.py
@@ -0,0 +1,7 @@
+from django.conf.urls import url
+
+from challenges.views import ChallengeList
+
+urlpatterns = [
+    url(r'^', ChallengeList.as_view(), name='list'),
+]
--- challenges/views.py
+++ challenges/views.py
@@ -1,1 +1,7 @@
-# Create your views here.
+from django.views.generic import ListView
+
+from challenges.models import Challenge
+
+
+class ChallengeList(ListView):
+    model = Challenge
