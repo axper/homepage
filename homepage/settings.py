4439f18b637562b91ec6b9da170e85846603c51c
Babken Vardanyan <babken.vardanyan@sflpro.com>
2017-08-23 12:17:31 +0400

    Reorganized static files

--- homepage/settings.py
+++ homepage/settings.py
@@ -101,8 +101,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
 ]
 
 # Where 'manage.py collectstatic' will copy files
-STATIC_ROOT = os.path.join(BASE_DIR, "staticroot")
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
