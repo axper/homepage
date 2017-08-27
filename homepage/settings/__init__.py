f04eb38879fb75d7b2efc8df9e10e537d51417da
Babken Vardanyan <babken.vardanyan@sflpro.com>
2017-08-27 19:20:06 +0400

    set concrete file names in django settings to retain order

--- homepage/settings/__init__.py
+++ homepage/settings/__init__.py
@@ -1,11 +1,12 @@
 from split_settings.tools import optional, include
 
 # This file contains default settings, development environment should "just work" without any changes
-# In production environment, we symlink 'available/production.py' to 'components/production.py'
 include(
-    'components/*.py',
+    'components/base.py',
+    # In production environment, we symlink 'available/production.py' to 'components/production.py'
+    optional('components/production.py'),
 
-    # Local settings, just in case we need somethign custom
+    # Local settings, just in case we need something custom
     optional('local_settings.py'),
 
     scope=locals()
