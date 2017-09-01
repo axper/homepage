2be20a25d87c37f9a1fc50004a598f77ccabb3d1
Babken Vardanyan <babken.vardanyan@sflpro.com>
2017-09-01 15:14:05 +0400

    formatting

--- homepage/settings/base.py
+++ homepage/settings/base.py
@@ -59,7 +59,9 @@ ROOT_URLCONF = 'homepage.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(PROJECT_DIR, 'templates')],
+        'DIRS': [
+            os.path.join(PROJECT_DIR, 'templates')
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
