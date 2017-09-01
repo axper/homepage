8320836370074fce95ddeabc25d9e26c6c4340cd
Babken Vardanyan <babken.vardanyan@sflpro.com>
2017-09-01 15:01:57 +0400

    rm unused import

--- homepage/urls.py
+++ homepage/urls.py
@@ -14,7 +14,6 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import include, url
-from django.conf.urls.static import static
 from django.contrib import admin
 from wagtail.wagtailadmin import urls as wagtailadmin_urls
 from wagtail.wagtaildocs import urls as wagtaildocs_urls
