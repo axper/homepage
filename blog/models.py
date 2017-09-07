5a6470dd15f6ff3ed704430d2d3d403d13eac45e
Babken Vardanyan <babken.vardanyan@sflpro.com>
2017-09-07 17:45:05 +0400

    Make resume page managed by wagtail's streamblock

--- blog/models.py
+++ blog/models.py
@@ -4,23 +4,112 @@ from django.db.models import TextField
 from modelcluster.contrib.taggit import ClusterTaggableManager
 from modelcluster.fields import ParentalKey, ParentalManyToManyField
 from taggit.models import TaggedItemBase
-from wagtail.wagtailadmin.edit_handlers import FieldPanel, InlinePanel, MultiFieldPanel
-from wagtail.wagtailcore.fields import RichTextField
+from wagtail.wagtailadmin.edit_handlers import FieldPanel, InlinePanel, MultiFieldPanel, StreamFieldPanel
+from wagtail.wagtailcore.blocks import StructBlock, CharBlock, RichTextBlock, StreamBlock, IntegerBlock, URLBlock
+from wagtail.wagtailcore.fields import RichTextField, StreamField
 from wagtail.wagtailcore.models import Page, Orderable
+from wagtail.wagtailimages.blocks import ImageChooserBlock
 from wagtail.wagtailimages.edit_handlers import ImageChooserPanel
 from wagtail.wagtailsearch import index
 from wagtail.wagtailsnippets.models import register_snippet
+
+
+class PortfolioCardBlock(StructBlock):
+    id = CharBlock(required=False)
+    title = CharBlock()
+    link = URLBlock()
+    image = ImageChooserBlock()
+    content = RichTextBlock()
+
+    class Meta:
+        template = 'blog/blocks/portfolio_card_block.html'
+
+
+class SmallGridBlock(StructBlock):
+    dividend = IntegerBlock(default=1)
+    divisor = IntegerBlock(default=2)
+    content = StreamBlock([
+        ('card_block', PortfolioCardBlock(blank=True)),
+    ])
+
+    class Meta:
+        template = 'blog/blocks/small_grid_block.html'
+
+
+class DescriptionListItemBlock(StructBlock):
+    data_title = CharBlock()
+    data_definition = CharBlock()
+
+    class Meta:
+        template = 'blog/blocks/description_list_item_block.html'
+
+
+class DescriptionListBlock(StructBlock):
+    content = StreamBlock([
+        ('description_item_block', DescriptionListItemBlock()),
+    ])
+
+    class Meta:
+        template = 'blog/blocks/description_list_block.html'
+
+
+class DownloadButtonBlock(StructBlock):
+    url = URLBlock()
+    text = CharBlock()
+
+    class Meta:
+        template = 'blog/blocks/download_button_block.html'
+
+
+class BaseSectionBlock(StructBlock):
+    id = CharBlock(required=False)
+    title = CharBlock()
+    content_blocks = [
+        ('rich_text', RichTextBlock(blank=True)),
+        ('small_grid_block', SmallGridBlock(blank=True)),
+        ('description_list_block', DescriptionListBlock(blank=True)),
+        ('download_button_block', DownloadButtonBlock(blank=True)),
+    ]
+
+
+class H4WithIdBlock(BaseSectionBlock):
+    class Meta:
+        template = 'blog/blocks/h4_with_id_block.html'
+
+
+class H3WithIdBlock(BaseSectionBlock):
+    # noinspection PyTypeChecker
+    content = StreamBlock(BaseSectionBlock.content_blocks + [
+        ('h4_section_block', H4WithIdBlock()),
+    ])
+
+    class Meta:
+        template = 'blog/blocks/h3_with_id_block.html'
+
+
+class SectionBlock(BaseSectionBlock):
+    # noinspection PyTypeChecker
+    content = StreamBlock(BaseSectionBlock.content_blocks + [
+        ('h3_section_block', H3WithIdBlock()),
+    ])
+
+    class Meta:
+        template = 'blog/blocks/section_block.html'
 
 
 class ResumePage(Page):
     name = TextField(default='Name Surname')
     sub_heading = TextField(default='Highly trained monkey')
     sub_sub_heading = TextField(default='Also a programmer')
+    body = StreamField([
+        ('section_block', SectionBlock()),
+    ], blank=True)
 
     content_panels = Page.content_panels + [
         FieldPanel('name'),
         FieldPanel('sub_heading'),
         FieldPanel('sub_sub_heading'),
+        StreamFieldPanel('body'),
     ]
 
 
